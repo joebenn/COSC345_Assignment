1db4e9a624d86ae09cc903314a6170d958bf6b0c
Joe Benn <joebenn95@gmail.com>
2016-08-09 14:37:17 +1200

    Store weather data in an array (#3)

--- weather_info_pull.js
+++ weather_info_pull.js
@@ -1,4 +1,5 @@
 var API_key = "8056edf51377902d28bd9e53a5dd97af";
+var weatherData = [];
 
 function success(pos) {
     var crd = pos.coords;
@@ -22,18 +23,14 @@ function getWeather(latitude, longitude) {
                     "&units=metric" + '&APPID=' + API_key, function (jsonObj) {
 
         var list = jsonObj.list;
-        for (i = 0; i < list.length; i++) {
-          console.log(list[i].weather[0].main);
-          console.log(list[i].weather[0].description);
-          console.log(list[i].dt_txt);
-          console.log(list[i].main.temp);
-          console.log("---------")
-}
-
-
-
-
-
+        for (i = 0; i < list.length; i++){
+          weatherData.push({main: list[i].weather[0].main, description: list[i].weather[0].description, date_time: list[i].dt_txt,
+                            temp: list[i].main.temp });
+        }
+        //prints weatherData to the console
+        for(i = 0; i < weatherData.length; i++){
+          console.log(weatherData[i]);
+        }
 
     });
 }
