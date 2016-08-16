2c973e6a0fc7b5cb3590389b9d37b902b634de58
Joe Benn <joebenn95@gmail.com>
2016-08-17 11:47:37 +1200

    Add files via upload

--- weather_info_pull.js
+++ weather_info_pull.js
@@ -23,10 +23,7 @@ function getWeather(latitude, longitude) {
   jQuery.getJSON('http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +
                     "&units=metric" + '&APPID=' + API_key, function (jsonObj) {
         var list = jsonObj.list;
-
-        // var city = jsonObj.city.name;
-        // console.log(city);
-        
+        var city = jsonObj.city;
         for (i = 0; i < list.length; i++){
           //find low and high for each day
           var date = new Date(list[i].dt*1000);
@@ -39,10 +36,7 @@ function getWeather(latitude, longitude) {
           var formattedTime = day + "/" + month + "/" + year + " "+ hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
             weatherData.push({main: list[i].weather[0].main, description: list[i].weather[0].description,
-                              date_time: formattedTime, temp: list[i].main.temp, city: jsonObj.city.name });
-
-
-
+                              date_time: formattedTime, temp: list[i].main.temp, city: city.name});
                               console.log(weatherData[i]);
 
          }
